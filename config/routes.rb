a14476caccd4c163abf2f4514febdcd679852660
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-03-16 15:17:27 +0100

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only: [ :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Golden Mango', address: 'Rue de Lausanne', category: 'chinese')
+Restaurant.create(name: 'Braseiro', address: 'Lucio Costa', category: 'french')
+Restaurant.create(name: 'Gurumê', address: 'Rio Design Barra', category: 'japanese')
+Restaurant.create(name: 'Italien', address: 'Ours', category: 'italian')
+Restaurant.create(name: 'Cafet', address: 'Renens', category: 'belgian')
